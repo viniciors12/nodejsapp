c5ef64bd546aa2e14e49afef38d7bc1bbabe6601
viniciors12 <viniciorodriguez97@gmail.com>
2024-04-24 16:06:13 -0600

    asd

--- DSL/jobscript.sh
+++ DSL/jobscript.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-echo "Bienvenido/a al planeta $planeta"
-if [ "$agente" = "true" ]
-then
-	echo "Prepárese para la misión agente X, que la fuerza te acompañe."
-else
-	echo "Disfruta el planeta humano/a $nombre, trata de no morir."
-fi
-echo "..."
-sleep 10
-echo "¡Suerte! La necesitarás."
+echo "Bienvenido/a al planeta $planeta, this is an example for $nombre"
